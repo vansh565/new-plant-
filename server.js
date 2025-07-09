49c097b4f57af1d80291ee3bdc5e8f851f4b63a8
vansh565 <sharmavansh969@gmail.com>
2025-07-09 13:19:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -48,9 +50,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/Plantwebsite", {
+mongoose.connect("mongodb+srv://sanjuuppal458:5Lciha7fjOz41VLB@cluster0.abcde.mongodb.net/Plantwebsite?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+
 }).then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.log("❌ MongoDB error", err));
 
